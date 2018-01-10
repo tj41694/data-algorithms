425e3b05d67420d484042344589b8e1d391ed153
tj41694 <tj41694@users.noreply.github.com>
2018-01-10 18:03:44 +0800

    2018-1-10
    优化不必要的递归调用

--- Sort_Quick/QuickSort.cpp
+++ Sort_Quick/QuickSort.cpp
@@ -2,7 +2,6 @@
 
 void swap(int* arr, int a, int b)
 {
-	//if (a == b) return; //a b相等不交换
 	int temp;
 	temp = arr[a];
 	arr[a] = arr[b];
@@ -11,7 +10,6 @@ void swap(int* arr, int a, int b)
 
 void QuickSort(int* arr, int left, int right)
 {
-	if (right - left < 2) return; //设置递归调用的终止条件，
 	int tempIndex = right + 1; //设置大于K值的最小索引位数，且同时兼具判断功能
 	for (int i = left + 1; i < right + 1; i++)
 	{
@@ -25,10 +23,12 @@ void QuickSort(int* arr, int left, int right)
 	}
 	/*如tempIndex == right +1，说明右边全部小于left值,自然与right进行交换
 	如tempIndex发生过改变，那么tempIndex - 1就是小于left的值中最右边的值*/
-	swap(arr, left, tempIndex - 1);
 	int k = tempIndex - 1;
-	QuickSort(arr, left, k - 1);
-	QuickSort(arr, k + 1, right);
+	swap(arr, left, k);
+	if (k - 1 - left > 1)
+		QuickSort(arr, left, k - 1);
+	if (right - k - 1 > 1)
+		QuickSort(arr, k + 1, right);
 }
 
 int main()
